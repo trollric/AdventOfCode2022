d56cd654132c7bd38d9a128570887994b1136249
Marcus Lilja <90m.hansson@gmail.com>
2022-12-03 17:06:52 +0100

    Wrote pseudocode.

---
+++ Day2/puzzle1.py
@@ -0,0 +1,33 @@
+"""
+Rock paper scissors.
+
+Points = (Value of selected shape + win/loss/draw)
+
+Shape | points
+Stone   = 1
+Paper   = 2
+Scissor = 3
+
+Win  = 6
+Draw = 3
+Loss = 0
+"""
+
+
+def point_calculator(player_shape : str, opponent_shape : str) -> int:
+    # Determine victor and add points.
+
+    # Add the value of the player_shape
+
+    # Return the value
+    pass
+
+
+if __name__ == '__main__':
+    # Read lines
+
+    # Clean the data
+
+    # Loop through each game
+        # Var +=  Send to a point calculator returning points.
+    pass
