17f5d7b29606957ef21bb35d565d0756ea5893c5
Marcus Lilja <90m.hansson@gmail.com>
2022-12-03 21:19:46 +0100

    Set up the question in the main file.

---
+++ Day3/puzzle1.py
@@ -0,0 +1,25 @@
+"""
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+
+The first rucksack contains the items vJrwpWtwJgWrhcsFMMfFFhFp,
+which means its first compartment contains the items vJrwpWtwJgWr,
+while the second compartment contains the items hcsFMMfFFhFp.
+The only item type that appears in both compartments is lowercase p.
+
+The second rucksack's compartments contain jqHRNqRjqzjGDLGL and rsFMfFZSrLrFZsSL.
+The only item type that appears in both compartments is uppercase L.
+
+The third rucksack's compartments contain PmmdzqPrV and vPwwTWBwg; the only common item type is uppercase P.
+
+The fourth rucksack's compartments only share item type v.
+
+The fifth rucksack's compartments only share item type t.
+
+The sixth rucksack's compartments only share item type s.
+"""
+
